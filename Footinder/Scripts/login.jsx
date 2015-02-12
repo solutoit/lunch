2c1d5d612bbd2f966097c32b3640175087671b8c
rotemsloo <rot253@gmail.com>
2015-02-12 16:56:51 +0200

    added login page

---
+++ Footinder/Scripts/login.jsx
@@ -0,0 +1,20 @@
+﻿var UserNameEditor = React.createClass({
+	render: function() {
+		return (
+			<form action="/DoLogin" method="post">
+				<input type="text" name="email" value="Type your soluto mail address" />
+				<button type="submit">Login</button>
+			</form>
+		);
+	}
+});
+
+var Page = React.createClass({
+	render: function() {
+		return (
+			<UserNameEditor />
+		);
+	}
+});
+
+React.render(<Page />,document.getElementById('content'));
