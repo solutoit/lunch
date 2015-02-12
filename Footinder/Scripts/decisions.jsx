26da5bd46a64d6c58bf1d8309d30c8db4ce26157
motiazu <spidermot@gmail.com>
2015-02-12 14:50:41 +0200

    Show pictures and decide

--- Footinder/Scripts/decisions.jsx
+++ Footinder/Scripts/decisions.jsx
@@ -1,6 +1,6 @@
 ﻿var RestaurantView = React.createClass({
 	render:function() {
-		if(!this.props.restaurant) return;
+		if(!this.props.restaurant) return <noscript/>;
 
 		return (
 		<div className="rest">
@@ -9,6 +9,22 @@
 				<div className="title">{this.props.restaurant.Name}</div>
 			</div>
 		</div>
+		);
+	}
+});
+
+var DecisionButtons = React.createClass({
+	decisionHandler:function(decision) {
+		this.props.decisionHandler(decision);
+	},
+	render:function() {
+		var onNo = this.decisionHandler.bind(this, false);
+		var onYes = this.decisionHandler.bind(this, true);
+		return (
+			<div class='decision-buttons'>
+				<button onClick={onNo}>no</button>
+				<button onClick={onYes}>yes</button>
+			</div>
 		);
 	}
 });
@@ -27,10 +43,19 @@ var App = React.createClass({
         if(this.state.currentRest < 0) return undefined;
         return this.state.restaurants[this.state.currentRest];
     },
+    decisionHandler: function(decision) {
+    	var curr = this.state.currentRest;
+    	if(curr == this.state.restaurants.length - 1) {
+    		console.log('go to results');
+    	}
+
+    	this.setState({currentRest: curr + 1});
+    },
 	render: function() {
 		return (
 		<div>
 			<RestaurantView restaurant={this.getCurrentRest()} />
+			<DecisionButtons decisionHandler={this.decisionHandler}/>
 		</div>
 		);
 	}
