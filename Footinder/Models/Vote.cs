b33f8ecef779f07cf751d0e11bec785aca71bf94
motiazu <spidermot@gmail.com>
2015-02-12 11:37:44 +0200

    hai

--- Footinder/Models/Vote.cs
+++ Footinder/Models/Vote.cs
@@ -9,7 +9,7 @@ namespace Footinder.Models
     public class Vote
     {
         public string Date { get; set; }
-        public string Restauarant { get; set; }
+        public string RestauarantId { get; set; }
 
         public bool Decision { get; set; }
         public string UserId { get; set; }
