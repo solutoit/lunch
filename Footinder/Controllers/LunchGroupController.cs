35c8c784f16e37071bf5dae8a075e3c48e5cf914
Dan Klausner <dan@soluto.com>
2015-02-12 15:40:28 +0200

    controller

---
+++ Footinder/Controllers/LunchGroupController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Footinder.Controllers
+{
+    public class LunchGroupController : Controller
+    {
+        //
+        // GET: /LunchGroup/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
